5850a1b80ca8cd04de34642708351ea501d011cb
Lakia Jones <rachandra_jones@yahoo.com>
2019-03-27 19:54:25 -0500

    WIP

--- main.js
+++ main.js
@@ -1,21 +1,69 @@
 console.log('Temperature-Converter');
 
-const toCelsius =  () => {
+const printToDom = (divId, textToPrint) => {
+    document.getElementById(divId).innerHTML = textToPrint;
+};
 
-}
+const c = document.getElementById('c');
+const f = document.getElementById('f');
+const clearButton = document.getElementById('clearButton');
+const convertButton = document.getElementById('convertButton');
+const tempInput = document.getElementById('tempInput');
+let value =0;
 
-const toFahrenheit =  () => {
+const toCelsius = () => {
+    value = (document.getElementById('tempInput').value - 32) * (5 / 9);
+    
+    let color = 'green';
+    if(value > 32) {
+        color = 'red';
+    }
+    else if( value < 0) {
+        color = 'blue';
+    }
+    document.getElementById('divOutput').style.color = color;
+    domStringBuilder = `<h2>${value} degrees C</h2>`
+    
+};
 
-}
+const toFahrenheit = () => {
+    value = (document.getElementById('tempInput').value * (9/5)) + 32;
 
-// Get a reference to the button element in the DOM
-const button = document.getElementById("converter");
+    let color = 'green';
+    if(value > 90) {
+        color = 'red';
+    }
+    else if(value < 32) {
+        color = 'blue';
+    }
+    document.getElementById('divOutput').style.color = color;
+    domStringBuilder = `<h2>${value} degrees C</h2>`
 
-// This function should determine which conversion should
-// happen based on which radio button is selected.
-const determineConverter = (e) => {
-  console.log("event", e);
-}
+};
 
-// Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
+const determineConverter = () => {
+    if(c.checked) {
+        toCelsius();
+        printToDom('divOutput', domStringBuilder);
+    }
+    else{
+        toFahrenheit();
+        printToDom('divOutput', domStringBuilder);
+    }
+};
+
+const clearTempInput = () => {
+    document.getElementById('tempInput').value = '';
+    document.getElementById('divOutput').textContent = '';
+    c.checked = false;
+    f.checked = false;
+};
+
+convertButton.addEventListener("click", determineConverter);
+clearButton.addEventListener("click", clearTempInput);
+
+document.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        document.getElementById('convertButton').click();
+    }
+});
